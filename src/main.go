d1a1d75202fb289cb806bf8a3808c7d414a75a77
JuchanPark <pjc7420@gmail.com>
2022-07-12 12:24:41 +0900

    test

--- src/main.go
+++ src/main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"go-mod/scrapper"
-	"strings"
-
-	"github.com/labstack/echo"
-)
-
-func handleHome(c echo.Context) error {
-	return c.File("home.html")
-}
-
-func handleScrape(c echo.Context) error {
-	term := strings.ToLower(scrapper.CleanSpace(c.FormValue("term")))
-	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "job.csv")
-}
+import "fmt"
 
 func main() {
-	e := echo.New()
-	e.GET("/", handleHome)
-	e.POST("/scrape", handleScrape)
-	e.Logger.Fatal(e.Start(":1323"))
-
+	fmt.Println("test")
 }
