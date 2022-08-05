163037f186dcfd8f9aedbe869f2507bdea770626
JuchanPark <pjc7420@gmail.com>
2022-08-05 17:37:06 +0900

    catch up

--- src/main.go
+++ src/main.go
@@ -1,35 +1,45 @@
 package main
 
 import (
-	"fmt"
 	"go-mod/blockchain"
-	"html/template"
 	"log"
 	"net/http"
+	"text/template"
 )
 
-const port string = ":3000"
+const (
+	port        string = ":3000"
+	templateDir string = "templates/"
+)
 
-type homedata struct {
+var templates *template.Template
+
+type homeData struct {
 	PageTitle string
 	Blocks    []*blockchain.Block
 }
 
-func main() {
-	chain := blockchain.Getblockchain()
-	chain.Addblock("Second block")
-	chain.Addblock("Third block")
-	chain.Addblock("Fourth block")
-	for _, block := range chain.Allblocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("PrevHash: %s\n", block.PrevHash)
-	}
+func home(rw http.ResponseWriter, r *http.Request) {
+	// tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
+	data := homeData{"Home", blockchain.Getblockchain().Allblocks()}
+	templates.ExecuteTemplate(rw, "home", data)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl := template.Must(template.ParseFiles("templates/home.gohtml"))
-		data := homedata{"Home", blockchain.Getblockchain().Allblocks()}
-		tmpl.Execute(w, data)
-	})
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+	http.HandleFunc("/", home)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
+
+// chain := blockchain.Getblockchain()
+// chain.Addblock("Second block")
+// chain.Addblock("Third block")
+// chain.Addblock("Fourth block")
+// for _, block := range chain.Allblocks() {
+// 	fmt.Printf("Data: %s\n", block.Data)
+// 	fmt.Printf("Hash: %s\n", block.Hash)
+// 	fmt.Printf("PrevHash: %s\n", block.PrevHash)
+// }
